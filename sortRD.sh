47ce8f743bdd304726f786328bd752f1814a563a
Alexey Kostarev <kaf@altlinux.org>
2025-12-11 00:02:53 +0500

    Change RDTree.md structure

--- sortRD.sh
+++ sortRD.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 dir="/tmp/getSubcommands_$$"
+# dir="/tmp/getSubcommands"
 mkdir $dir
 rm -f $dir/*
 
@@ -10,15 +11,25 @@ do
   IFS=.
   set -- $ID
   LEVEL1=$2
+  LEVEL2=$1
   IFS=$ifs
 #   echo $LEVEL1 $ALIASES
-  echo $ALIASES >> $dir/$LEVEL1
+  echo $LEVEL2 $ALIASES >> $dir/$LEVEL1
 done
-
+# exit
 cd $dir
 for NS in *
 do
   echo "${NS}:"
-  sed -e 's/^/  - /' $NS
+  echo "  name: $NS"
+  echo "  commands:"
+  while read id name aliases
+  do
+    echo "  - id: $id"
+    echo "    aliases: $name $aliases"
+    echo "    command: $name"
+    echo "    name: $name"
+  done < $NS
+#   sed -e 's/^/    - /' $NS
 done
 rm -rf $dir
