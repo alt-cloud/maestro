d6105f57cc0e0f77c1c0f31148e68d2c0e992123
Alexey Kostarev <kaf@altlinux.org>
2026-01-08 18:18:37 +0500

    Add first releade of headlamp plugin: support seperate MASTRIo WINDOW and list cueters from static src/.kube/config file

---
+++ plugin/init.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+npx --yes @kinvolk/headlamp-plugin create maestro
+cd maestro
+npm install
---
+++ plugin/maestro/src/index.tsx
@@ -0,0 +1,212 @@
+/*
+ * Copyright 2025 The Kubernetes Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// import { registerAppBarAction } from '@kinvolk/headlamp-plugin/lib';
+
+// Below are some imports you may want to use.
+//   See README.md for links to plugin development documentation.
+// import { Headlamp, K8s, useTranslation } from '@kinvolk/headlamp-plugin/lib';
+// import { SectionBox } from '@kinvolk/headlamp-plugin/lib/CommonComponents';
+// import { K8s } from '@kinvolk/headlamp-plugin/lib/K8s';
+// import { Typography } from '@mui/material';
+
+// registerAppBarAction(<span>Hello from maestro</span>);
+
+// Example of using i18n (internationalization):
+// function MyComponent() {
+//   const { t } = useTranslation();
+//   return <div>{t('translation_key')}</div>;
+// }
+
+
+import {
+  registerRoute,
+  registerRouteFilter,
+  registerSidebarEntry,
+  registerSidebarEntryFilter,
+} from '@kinvolk/headlamp-plugin/lib';
+import { SectionBox } from '@kinvolk/headlamp-plugin/lib/CommonComponents';
+import Typography from '@mui/material/Typography';
+import React from 'react';
+import MaestroIcon from './maestro.svg';
+
+import { useState } from 'react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  TablePagination,
+  TableSortLabel
+} from '@mui/material';
+import { Link } from 'react-router-dom';
+
+import yamlText from './manifests/config.yaml?raw';
+import * as yaml from 'js-yaml';
+
+interface Cluster {
+  id: number;
+  name: string;
+}
+
+interface Column {
+  id: keyof Cluster;
+  label: string;
+  sortable?: boolean;
+}
+
+const columns: Column[] = [
+  { id: 'name', label: 'Name', sortable: true }
+];
+
+async function loadYamlByName(fileName: string) {
+  if (yamlFiles[fileName]) {
+    const text = await yamlFiles[fileName]();
+    return yaml.load(text);
+  }
+  throw new Error(`Unknown YAML file: ${fileName}`);
+  }
+
+function ClusterTable() {
+  const fileName = '.kube/config';
+  const data = yaml.load(fileName);
+  alert(data);
+  const [rows, setRows] = useState<Cluster[]>([
+    { id: 1, name: <Link to="/maestro/cluster/Maestro">admin@Maestro</Link> }
+  ]);
+
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [orderBy, setOrderBy] = useState<keyof Cluster>('name');
+  const [order, setOrder] = useState<'asc' | 'desc'>('asc');
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  const handleSort = (property: keyof Cluster) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const sortedRows = [...rows].sort((a, b) => {
+    if (a[orderBy] < b[orderBy]) return order === 'asc' ? -1 : 1;
+    if (a[orderBy] > b[orderBy]) return order === 'asc' ? 1 : -1;
+    return 0;
+  });
+
+  const paginatedRows = sortedRows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
+
+  return (
+    <Paper>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              {columns.map(column => (
+                <TableCell key={column.id}>
+                  {column.sortable ? (
+                    <TableSortLabel
+                      active={orderBy === column.id}
+                      direction={orderBy === column.id ? order : 'asc'}
+                      onClick={() => handleSort(column.id)}
+                    >
+                      {column.label}
+                    </TableSortLabel>
+                  ) : (
+                    column.label
+                  )}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {paginatedRows.map(row => (
+              <TableRow key={row.id}>
+                <TableCell>{row.name}</TableCell>
+                <TableCell>{row.age}</TableCell>
+                <TableCell>{row.email}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
+
+// Add an entry to the home sidebar (not in cluster).
+registerSidebarEntry({
+  name: 'maestroplugin',
+  label: 'Maestro',
+  url: '/maestro',
+  icon: 'MaestroIcon',
+  sidebar: 'HOME',
+});
+
+
+registerRoute({
+  path: '/maestro',
+  sidebar: {
+    item: 'maestro',
+    sidebar: 'myplugin',
+  },
+  useClusterURL: false,
+  noAuthRequired: true, // No authentication is required to see the view
+  name: 'maestro',
+  exact: true,
+  component: () => (
+    <SectionBox title="MAESTRO" textAlign="left" paddingTop={2}>
+      <Typography fontSize="24px" fontWeight="bold">Maestro section</Typography>
+      <ClusterTable/>
+    </SectionBox>
+  ),
+});
+// Adds a completely new sidebar + entry because the sidebar "myplugin" does not exist.
+registerSidebarEntry({
+  name: 'backtoclusters',
+  label: 'Back to Clusters',
+  url: '/',
+  icon: 'mdi:hexagon',
+  sidebar: 'myplugin',
+});
+// Adds a entry to the recently created sidebar "maestro".
+registerSidebarEntry({
+  name: 'maestro',
+  label: 'MAESTRO AREA',
+  url: '/maestro',
+  icon: 'MaestroIcon',
+  sidebar: 'myplugin',
+});
+
