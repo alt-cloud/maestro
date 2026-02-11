50f83ee36620569c2d115b0a08c269b8bec8f919
Alexey Kostarev <kaf@altlinux.org>
2026-02-11 16:10:30 +0500

    Transition from nodeTypes.json to talosconfig nodes list

--- api/flaskAPI.py
+++ api/flaskAPI.py
@@ -8,6 +8,8 @@ import json
 from flask_cors import CORS
 import socket
 import time
+import yaml
+from pathlib import Path
 
 app = Flask(__name__)
 # CORS(app, origins=["http://localhost:3000"])
@@ -72,6 +74,16 @@ def tableToJson(str):
         vals[columnName] = row[start:end].strip()
     rows.append(vals)
   return json.dumps(rows, indent=2)
+
+def runShellCommand(runCmd, homedir):
+  print('runShellCommand=', runCmd)
+  result = subprocess.run(runCmd,
+    shell=True,
+    stdout=subprocess.PIPE,
+    cwd='%s/.maestro/' % homedir,
+    encoding='utf-8'
+  )
+  return result
 
 @app.route('/talosctl')
 def talosctl():
@@ -378,8 +390,183 @@ def nodesList(nmapStr):
     ret[nodeState['ip']] = nodeState
   return ret
 
-@app.route('/scanNets',methods=['GET', 'POST'])
+def isMaintenance(ip):
+  homedir = os.getenv('HOME')
+  runCmd = 'talosctl get  discoveredvolume -o json -n %s -e %s -i' % (ip, ip)
+  print('runCmd=', runCmd)
+  result = subprocess.run(runCmd,
+    shell=True,
+    stdout=subprocess.PIPE,
+    cwd='%s/.maestro/' % homedir,
+    encoding='utf-8'
+  )
+  result = json.loads('[' + result.stdout.replace("}\n{","},{") + ']')
+  for volInfo in result:
+    if 'partition_label' in volInfo['spec']:
+      return False
+  return True
 
+
+def talosgetspec(subcmd, node, insecure):
+  homedir = os.getenv('HOME')
+  runCmd = 'talosctl get %s -e %s -n %s -o json %s' % (subcmd, node, node, insecure)
+  print('RUNCmd=', runCmd)
+  result = subprocess.run(runCmd,
+    shell=True,
+    stdout=subprocess.PIPE,
+    cwd='%s/.maestro/' % homedir,
+    encoding='utf-8'
+  )
+  returncode = result.returncode
+  ret = ''
+  if returncode == 0:
+    jsonStr = result.stdout.strip()
+    if len(jsonStr) != 0:
+      jsonDict = json.loads(jsonStr)
+      ret = jsonDict['spec']
+  return [ret, returncode]
+
+def nodeClusterName(node):
+  [spec, returncode] = talosgetspec('info', node, '')
+  # print('clusterName=%s returncode=%d ' % (clusterName, returncode))
+  print('spec=', spec)
+  print('returncode=', returncode)
+  if returncode != 0:
+    clusterName = '_Orphans'
+    insecure = '-i'
+    if not isMaintenance(node):
+      clusterName = '_Unknown'
+  else:
+    clusterName = spec['clusterName']
+    insecure = ''
+  return [insecure, clusterName]
+
+def is_port_open(host: str, port: int, timeout: float = 3.0) -> bool:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(timeout)
+    result = sock.connect_ex((host, port))  # возвращает 0 при успехе, иначе errno
+    sock.close()
+    return result == 0
+
+def refreshTalosconfig():
+  homedir = os.getenv('HOME')
+  maestroConfigDir =  '%s/.maestro' % homedir
+  print('refreshTalosconfig:: Before: maestroConfigDir=%s' % maestroConfigDir)
+  talosconfigFile = '%s/talosconfig' % maestroConfigDir
+  fp = open(talosconfigFile, 'r')
+  talosconfig = yaml.safe_load(fp)
+  fp.close()
+  print('refreshTalosconfig:: Before: talosconfig=%s' % json.dumps(talosconfig, indent=2))
+  newNodes = {'_Orphans': {'endpoints': [], 'nodes': []} , '_Unknown': {'endpoints': [], 'nodes': []} }
+  changed = False
+  for contextName in talosconfig['contexts']:
+    endpoints = talosconfig['contexts'][contextName]['endpoints']\
+      if 'endpoints' in talosconfig['contexts'][contextName] else []
+    for ip in endpoints:
+      if is_port_open(ip, 50000):
+        [insecure, toClusterName] = nodeClusterName(ip)
+        if toClusterName not in newNodes:
+          newNodes[toClusterName] = {'endpoints': [], 'nodes': []}
+        if is_port_open(ip, 6443): # endpoint остается в endpoint
+          newNodes[toClusterName]['endpoints'].append(ip)
+          changed = contextName == toClusterName if not changed else False
+        else: # endpoint перешел в node состояние
+          newNodes[toClusterName]['nodes'].append(ip)
+          changed = True
+      else: # endpoint в init режиме
+        if contextName not in newNodes:
+          newNodes[contextName] = {'endpoints': [], 'nodes': []}
+        newNodes[contextName]['endpoints'].push(ip)
+
+    nodes = talosconfig['contexts'][contextName]['nodes']\
+      if 'nodes' in talosconfig['contexts'][contextName] else []
+    for ip in nodes:
+      if is_port_open(ip, 50000):
+        [insecure, toClusterName] = nodeClusterName(ip)
+        if toClusterName not in newNodes:
+          newNodes[toClusterName] = {'endpoints': [], 'nodes': []}
+        if is_port_open(ip, 6443): # node перешел в endpoint
+          newNodes[toClusterName]['endpoints'].append(ip)
+          changed = True
+        else: # node остался node
+          newNodes[toClusterName]['nodes'].append(ip)
+          changed = contextName == toClusterName if not changed else False
+      else:  # worker in init mode
+        if contextName not in newNodes:
+          newNodes[contextName] = {'endpoints': [], 'nodes': []}
+        newNodes[contextName]['nodes'].push(ip)
+  print('refreshTalosconfig:: After:  talosconfig=%s' % json.dumps(talosconfig, indent=2))
+  for clusterName in newNodes:
+    endpoints = list(set(newNodes[clusterName]['endpoints'])) # uniq values
+    if endpoints != newNodes[clusterName]['endpoints']:
+      newNodes[clusterName]['endpoints'] = endpoints
+      changed = True
+    nodes = list(set(newNodes[clusterName]['nodes'])) # uniq values
+    if nodes != newNodes[clusterName]['nodes']:
+      newNodes[clusterName]['nodes'] = nodes
+      changed = True
+  print('refreshTalosconfig:: newNodes=%s' % json.dumps(newNodes, indent=2))
+  print('refreshTalosconfig:: changed=', changed)
+  if changed:
+    print('refreshTalosconfig:: talosctl changed')
+    for clusterName in newNodes:
+      # runShellCommand('talosctl config context %s' % clusterName, homedir)
+      endpoints = newNodes[clusterName]['endpoints']
+      nodes = newNodes[clusterName]['nodes']
+      # print('refreshTalosconfig:: clusterName=%s endpoints:' % (clusterName, endpoints)
+      # print('refreshTalosconfig:: clusterName=%s nodes:' % clusterName, nodes)
+      runShellCommand("yq -yi '.contexts.%s.endpoints=%s' talosconfig" % (clusterName, json.dumps(endpoints)), homedir)
+      runShellCommand("yq -yi '.contexts.%s.nodes=%s' talosconfig" % (clusterName, json.dumps(nodes)), homedir)
+      # runShellCommand('talosctl config endpoint %s' % ' '.join(endpoints), homedir)
+      # runShellCommand('talosctl config node %s' % ' '.join(nodes), homedir)
+
+def initTalosconfig(nodes):
+  homedir = os.getenv('HOME')
+  maestroConfigDir =  '%s/.maestro' % homedir
+  fp = open(talosconfigFile, 'w')
+  emptyContent = '''context: _Orphans
+contexts:
+  _Orphans:
+    endpoints: []
+    nodes: []
+  _Unknown:
+    endpoints: []
+    nodes: %s
+'''
+  fp.write(emptyContent % json.dimps(nodes))
+  fp.close()
+  start_dir = Path(".")
+  for item in start_dir.iterdir():
+    if item.is_dir():
+      # print(item.name)
+      talosconfigFile = '%s/talosconfig' % item.name
+      if os.path.exists(talosconfigFile):
+        print('initTalosconfig:: merge subTalosconfig=%s' % talosconfigFile)
+        runCmd = 'talosctl config merge %s' % talosconfigFile
+        runShellCommand(runCmd, homedir)
+  refreshTalosconfig()
+
+# def refreshNodeTypes(nodeTypes, nodeTypesFile):
+#   ret = {}
+#   ret['controlplanes'] = []
+#   ret['workers'] = []
+#   for node in nodeTypes['controlplanes']+nodeTypes['workers']:
+#     if is_port_open(node, 50000):
+#       if is_port_open(node, 6443):
+#         ret['controlplanes'].append(node)
+#       else:
+#         ret['workers'].append(node)
+#   if set(ret['controlplanes']) != set(ret['controlplanes']) or \
+#      set(ret['workers']) != set(ret['workers']):
+#     print('%s updated' % nodeTypesFile)
+#     fp = open(nodeTypesFile, 'w')
+#     json.dump(ret, fp, indent=2)
+#     fp.close()
+#   else:
+#     print('%s unchanged' % nodeTypesFile)
+#   return ret
+
+@app.route('/scanNets',methods=['GET', 'POST'])
 def scanNets():
   homedir = os.getenv('HOME')
   print('REQUEST=', request.method);
@@ -414,141 +601,150 @@ def scanNets():
   nodes = nodesList(nmapOut.strip())
   print('NODES=', json.dumps(nodes, indent=4))
 
-  nodeTypes = {'controlplanes': [], 'workers': []}
+  initTalosconfig(nodes)
+
+  # talosconfigFile = maestroConfigDir + '/talosconfig'
+  # with open(talosconfigFile, 'w') as fp:
+  #     fp.write('contexts:')
+  # for virtualCluster in ['_Orphans', '_Unknown']:
+  #   runCmd = 'talosctl config add %s' % virtualCluster
+  #   runShellCommand(runCmd, homedir)
+
+  # nodeTypes = {'controlplanes': [], 'workers': []}
+  nodesTree = {}
+  for virtualCluster in ['_Orphans', '_Unknown']:
+    nodesTree[virtualCluster] = {}
+    for nodeType in [ 'endpoint', 'node']:
+      nodesTree[virtualCluster][nodeType] = []
+
   for ip in nodes:
+    maintenance = isMaintenance(ip)
     node = nodes[ip]
     if node['apidState'] == 'open':
       if node['kubeState'] == 'open':
-        nodeTypes['controlplanes'].append(ip)
+        # nodeTypes['controlplanes'].append(ip)
+        nodesTree['_Unknown']['endpoint'].append(ip)
       else:
-        nodeTypes['workers'].append(ip)
-  print('nodeTypes=', nodeTypes)
-  nodeTypesFile = maestroConfigDir + '/nodeTypes.json'
-  fp = open(nodeTypesFile, 'w')
-  json.dump(nodeTypes, fp, indent=2)
-  fp.close()
+        if maintenance:
+          nodesTree['_Orphans']['node'].append(ip)
+        else:
+          nodesTree['_Unknown']['node'].append(ip)
+        # nodeTypes['workers'].append(ip)
+  print('nodesTree=', json.dumps(nodesTree, indent=4))
+  for virtualCluster in ['_Orphans', '_Unknown']:
+    for nodeType in [ 'endpoint', 'node']:
+      nodes = nodesTree[virtualCluster][nodeType]
+      runShellCommand("yq '.contexts.%s.%ss=%s" % (virtualCluster, nodeType, json.dumps(nodes)), homedir),
+      # runShellCommand('talosctl config context %s' % virtualCluster, homedir)
+      # runShellCommand('talosctl config %s %s' % (nodeType, ' '.join(nodes)), homedir)
+  # print('nodeTypes=', nodeTypes)
+  # nodeTypesFile = maestroConfigDir + '/nodeTypes.json'
+  # fp = open(nodeTypesFile, 'w')
+  # json.dump(nodeTypes, fp, indent=2)
+  # fp.close()
 
   return jsonify({
         "status": "success",
         "message": f"Successfully scanned"
     }), 200
 
-def talosgetspec(subcmd, node, insecure):
-  homedir = os.getenv('HOME')
-  runCmd = 'talosctl get %s -e %s -n %s -o json %s' % (subcmd, node, node, insecure)
-  print('RUNCmd=', runCmd)
-  result = subprocess.run(runCmd,
-    shell=True,
-    stdout=subprocess.PIPE,
-    cwd='%s/.maestro/' % homedir,
-    encoding='utf-8'
-  )
-  returncode = result.returncode
-  ret = ''
-  if returncode == 0:
-    jsonStr = result.stdout.strip()
-    if len(jsonStr) != 0:
-      jsonDict = json.loads(jsonStr)
-      ret = jsonDict['spec']
-  return [ret, returncode]
-
-def is_port_open(host: str, port: int, timeout: float = 3.0) -> bool:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(timeout)
-    result = sock.connect_ex((host, port))  # возвращает 0 при успехе, иначе errno
-    sock.close()
-    return result == 0
-
-def refreshNodeTypes(nodeTypes, nodeTypesFile):
-  ret = {}
-  ret['controlplanes'] = []
-  ret['workers'] = []
-  for node in nodeTypes['controlplanes']+nodeTypes['workers']:
-    if is_port_open(node, 50000):
-      if is_port_open(node, 6443):
-        ret['controlplanes'].append(node)
-      else:
-        ret['workers'].append(node)
-  if set(ret['controlplanes']) != set(ret['controlplanes']) or \
-     set(ret['workers']) != set(ret['workers']):
-    print('%s updated' % nodeTypesFile)
-    fp = open(nodeTypesFile, 'w')
-    json.dump(ret, fp, indent=2)
-    fp.close()
-  else:
-    print('%s unchanged' % nodeTypesFile)
-  return ret
-
-def isMaintenance(ip):
-  homedir = os.getenv('HOME')
-  runCmd = 'talosctl get  discoveredvolume -o json -n %s -e %s -i' % (ip, ip)
-  print('runCmd=', runCmd)
-  result = subprocess.run(runCmd,
-    shell=True,
-    stdout=subprocess.PIPE,
-    cwd='%s/.maestro/' % homedir,
-    encoding='utf-8'
-  )
-  result = json.loads('[' + result.stdout.replace("}\n{","},{") + ']')
-  for volInfo in result:
-    if 'partition_label' in volInfo['spec']:
-      return False
-  return True
-
 @app.route('/nodesTree')
 def nodesTree():
   homedir = os.getenv('HOME')
   configDir = homedir + '/.maestro'
-  nodeTypesFile = configDir + '/nodeTypes.json'
-  if not os.path.isfile(nodeTypesFile):
-    return {}
-  fp = open(nodeTypesFile, 'r')
-  nodeTypes = json.load(fp)
-  fp.close()
-  # print('nodeTypes=', nodeTypes)
-  nodeTypes = refreshNodeTypes(nodeTypes, nodeTypesFile)
+  print('refreshTalosconfig:: Call')
+  refreshTalosconfig()
   nodesTree = {}
-  for nodeType in ['controlplanes', 'workers']:
-    for node in nodeTypes[nodeType]:
-      print(nodeType, node)
-      [spec, returncode] = talosgetspec('info', node, '')
-      # print('clusterName=%s returncode=%d ' % (clusterName, returncode))
-      print('spec=', spec)
-      print('returncode=', returncode)
-      if returncode != 0:
-        clusterName = '_Orphans'
-        insecure = '-i'
-        if not isMaintenance(node):
-          clusterName = '_Unknown'
-      else:
-        clusterName = spec['clusterName']
-        insecure = ''
-      if clusterName not in nodesTree:
-        nodesTree[clusterName] = { 'controlplanes': [], 'workers': [] }
-      nodeInfo = {}
-      nodeInfo['ip'] = node
-      # print('clusterName=%s' % clusterName)
-      # print('SPEC=%s' % json.dumps(spec))
-      # print('len(SPEC)=%d' % len(spec))
-      # print("COND=",  clusterName != '_Unknown' and len(spec) > 2)
-      if clusterName != '_Unknown' and len(spec) > 0:
-        [spec, returncode] = talosgetspec('machinestatus', node, insecure)
-        nodeInfo['stage'] = spec['stage']
-        nodeInfo['status'] = spec['status']
-        [spec, returncode] = talosgetspec('nodestatus', node, insecure)
-        if returncode == 0 :
-          nodeInfo['nodeReady'] = spec['nodeReady'] if 'nodeReady' in spec else '-'
-          [spec, returncode] = talosgetspec('manifeststatus', node, insecure)
-          nodeInfo['manifestsApplied'] = spec['manifestsApplied'] if 'manifestsApplied' in spec else []
-          [spec, returncode] = talosgetspec('etcdmember', node, insecure)
-          nodeInfo['memberID'] = spec['memberID'] if 'memberID' in spec else '-'
-      elif clusterName == '_Orphans':
-        nodeInfo['stage'] = 'maintenance'
-      nodesTree[clusterName][nodeType].append(nodeInfo)
-  if '_Orphans' in nodesTree:
-    nodesTree['_Orphans']['controlplanes'] = nodesTree['_Orphans']['workers']
-    nodesTree['_Orphans']['workers'] = []
+  print('nodesTree:: configDir=%s' % configDir)
+  talosconfigFile = '%s/talosconfig' % configDir
+  fp = open(talosconfigFile, 'r')
+  talosconfig = yaml.safe_load(fp)
+  fp.close()
+  print('nodesTree:: talosconfig=%s' % json.dumps(talosconfig, indent=2))
+  nodeTypesAliases = {'endpoints': 'controlplanes', 'nodes': 'workers'}
+  for clusterName in talosconfig['contexts']:
+    nodesTree[clusterName] = { 'controlplanes': [], 'workers': [] }
+    for nodeType in ['endpoints', 'nodes']:
+      nodes = talosconfig['contexts'][clusterName][nodeType] if nodeType in talosconfig['contexts'][clusterName] else []
+      for node in nodes:
+        nodeInfo = {}
+        nodeInfo['ip'] = node
+        [spec, returncode] = talosgetspec('info', node, '')
+        print('clusterName=%s returncode=%d ' % (clusterName, returncode))
+        if returncode != 0:
+          clusterName = '_Orphans'
+          insecure = '-i'
+          if not isMaintenance(node):
+            clusterName = '_Unknown'
+        else:
+          clusterName = spec['clusterName']
+          insecure = ''
+        if clusterName != '_Unknown' and len(spec) > 0:
+          [spec, returncode] = talosgetspec('machinestatus', node, insecure)
+          nodeInfo['stage'] = spec['stage']
+          nodeInfo['status'] = spec['status']
+          [spec, returncode] = talosgetspec('nodestatus', node, insecure)
+          if returncode == 0 :
+            nodeInfo['nodeReady'] = spec['nodeReady'] if 'nodeReady' in spec else '-'
+            [spec, returncode] = talosgetspec('manifeststatus', node, insecure)
+            nodeInfo['manifestsApplied'] = spec['manifestsApplied'] if 'manifestsApplied' in spec else []
+            [spec, returncode] = talosgetspec('etcdmember', node, insecure)
+            nodeInfo['memberID'] = spec['memberID'] if 'memberID' in spec else '-'
+        elif clusterName == '_Orphans':
+          nodeInfo['stage'] = 'maintenance'
+        nodeTypesAlias = nodeTypesAliases[nodeType]
+        nodesTree[clusterName][nodeTypesAlias].append(nodeInfo)
   return nodesTree
+
+  # nodeTypesFile = configDir + '/nodeTypes.json'
+  # if not os.path.isfile(nodeTypesFile):
+  #   return {}
+  # fp = open(nodeTypesFile, 'r')
+  # nodeTypes = json.load(fp)
+  # fp.close()
+  # # print('nodeTypes=', nodeTypes)
+  # nodeTypes = refreshNodeTypes(nodeTypes, nodeTypesFile)
+  # for nodeType in ['controlplanes', 'workers']:
+  #   for node in nodeTypes[nodeType]:
+  #     print(nodeType, node)
+  #     [spec, returncode] = talosgetspec('info', node, '')
+  #     # print('clusterName=%s returncode=%d ' % (clusterName, returncode))
+  #     print('spec=', spec)
+  #     print('returncode=', returncode)
+  #     if returncode != 0:
+  #       clusterName = '_Orphans'
+  #       insecure = '-i'
+  #       if not isMaintenance(node):
+  #         clusterName = '_Unknown'
+  #     else:
+  #       clusterName = spec['clusterName']
+  #       insecure = ''
+  #     if clusterName not in nodesTree:
+  #       nodesTree[clusterName] = { 'controlplanes': [], 'workers': [] }
+  #     nodeInfo = {}
+  #     nodeInfo['ip'] = node
+  #     # print('clusterName=%s' % clusterName)
+  #     # print('SPEC=%s' % json.dumps(spec))
+  #     # print('len(SPEC)=%d' % len(spec))
+  #     # print("COND=",  clusterName != '_Unknown' and len(spec) > 2)
+  #     if clusterName != '_Unknown' and len(spec) > 0:
+  #       [spec, returncode] = talosgetspec('machinestatus', node, insecure)
+  #       nodeInfo['stage'] = spec['stage']
+  #       nodeInfo['status'] = spec['status']
+  #       [spec, returncode] = talosgetspec('nodestatus', node, insecure)
+  #       if returncode == 0 :
+  #         nodeInfo['nodeReady'] = spec['nodeReady'] if 'nodeReady' in spec else '-'
+  #         [spec, returncode] = talosgetspec('manifeststatus', node, insecure)
+  #         nodeInfo['manifestsApplied'] = spec['manifestsApplied'] if 'manifestsApplied' in spec else []
+  #         [spec, returncode] = talosgetspec('etcdmember', node, insecure)
+  #         nodeInfo['memberID'] = spec['memberID'] if 'memberID' in spec else '-'
+  #     elif clusterName == '_Orphans':
+  #       nodeInfo['stage'] = 'maintenance'
+  #     nodesTree[clusterName][nodeType].append(nodeInfo)
+  # if '_Orphans' in nodesTree:
+  #   nodesTree['_Orphans']['controlplanes'] = nodesTree['_Orphans']['workers']
+  #   nodesTree['_Orphans']['workers'] = []
+  # return nodesTree
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=False)
