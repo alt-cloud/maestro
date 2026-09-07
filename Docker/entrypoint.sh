08cd20e9515c9c282b8a5c04336fd362eb9e90d3
Alexey Kostarev <kaf@altlinux.org>
2026-09-08 00:51:05 +0500

    Modify Docker/entrypoint.sh

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,57 +1,59 @@
 #!/bin/sh
 set -x
-
-
-if [ -z "$MAESTRO_DEPLOYMENT_MODE" ]
+# export HOME="/home/maestro"
+if [ $(id -u maestro) -eq "$MAESTRO_API_UID" ]
 then
-  MAESTRO_DEPLOYMENT_MODE="frontend backend"
+  MAESTRO_API_USER='maestro'
+else
+  MAESTRO_API_USER='maestro'
+  userdel $MAESTRO_API_USER
+  useradd -M -u $MAESTRO_API_UID -d /home/maestro $MAESTRO_API_USER
+  chown $MAESTRO_API_UID /home/maestro/.maestro /home/maestro/.kube
 fi
 
-# caddy run --config /etc/caddy/Caddyfile &
+# Frontend
+if [ -z "$MAESTRO_FRONTEBD_HOST" ]
+then
+  MAESTRO_FRONTEBD_HOST=127.0.0.1
+fi
+if [ -z "$MAESTRO_API_URL" ]
+then
+  MAESTRO_API_URL='http://127.0.0.1:5000'
+fi
 
-for mode in $MAESTRO_DEPLOYMENT_MODE
+echo 'root ALL=(ALL) ALL' >/etc/sudoers.d/root
+echo "{\"MAESTRO_API_URL\": \"$MAESTRO_API_URL\"}" | jq . > /usr/share/headlamp/plugins/maestro/config.json
+
+cmd="/usr/bin/headlamp-server \
+    -listen-addr $MAESTRO_FRONTEBD_HOST\
+    -port 4466\
+    -html-static-dir /usr/share/headlamp/frontend\
+    -plugins-dir /usr/share/headlamp/plugins"
+while true
 do
-  case $mode in
-  'frontend')
-    if [ -z "$MAESTRO_FRONTEBD_HOST" ]
-    then
-      MAESTRO_FRONTEBD_HOST=127.0.0.1
-    fi
-    if [ -z "$MAESTRO_API_URL" ]
-    then
-      MAESTRO_API_URL='http://127.0.0.1:5000'
-    fi
-    echo "{\"MAESTRO_API_URL\": \"$MAESTRO_API_URL\"}" | jq . > /usr/share/headlamp/plugins/maestro/config.json
-    KUBECONFIG=~/.kube/config
-    su - -c "/usr/bin/headlamp-server \
-      -listen-addr $MAESTRO_FRONTEBD_HOST\
-      -port 4466\
-      -html-static-dir /usr/share/headlamp/frontend\
-      -plugins-dir /usr/share/headlamp/plugins" maestro  >&2  &
-    ;;
-  'backend')
-    echo 'root ALL=(ALL) ALL' >/etc/sudoers.d/root
-    VARS=$(env | grep '^MAESTRO_' | cut -d= -f1 | paste -sd, -)
-    if [ $(id -u maestro) -eq "$MAESTRO_API_UID" ]
-    then
-      MAESTRO_API_USER='maestro'
-    elif [ $MAESTRO_API_UID -eq 0 ]
-    then
-      MAESTRO_API_USER='root'
-    else
-      MAESTRO_API_USER='maestro-api'
-      useradd -u $MAESTRO_API_UID -d /home/maestro $MAESTRO_API_USER
-      chown $MAESTRO_API_UID /home/maestro/.maestro /home/maestro/.kube
-    fi
-    source /home/maestro/maestro_api/.venv/bin/activate
-    set -a
-    while true
-    do
-      sudo -u $MAESTRO_API_USER --preserve-env=$VARS,PATH /home/maestro/maestro_api/.venv/bin/maestro-api >&2
-      echo 'Restart maestro API'
-    done &
-    ;;
-  esac
-done
+  if [ "$container" = 'podman' ]
+  then
+    $cmd >&2
+  else
+    sudo -u $MAESTRO_API_USER  $cmd >&2
+  fi
+  echo 'Restart headlamp plugin'
+  sleep 1
+done &
+# Backend
+VARS=$(env | grep '^MAESTRO_' | cut -d= -f1 | paste -sd, -)
+source /home/maestro/maestro_api/.venv/bin/activate
+cmd="/home/maestro/maestro_api/.venv/bin/maestro-api"
+while true
+do
+  if [ "$container" = 'podman' ]
+  then
+    $cmd >&2
+  else
+    sudo -u $MAESTRO_API_USER --preserve-env=$VARS,HOME,PATH,UID $cmd >&2
+  fi
+  echo 'Restart maestro API'
+  sleep 1
+done &
 
 sleep infinity
