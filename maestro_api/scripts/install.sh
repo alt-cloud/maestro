e8ffc93519d10a63ab2dc149c74c4740fd6c2d7f
Alexey Kostarev <kaf@altlinux.org>
2026-09-22 12:03:59 +0500

    Remove unused package python3-module-virtualenv in maestro-api/scripts/install.sh

--- maestro_api/scripts/install.sh
+++ maestro_api/scripts/install.sh
@@ -5,11 +5,9 @@ SCRIPT_DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
 API_DIR="$(dirname "$SCRIPT_DIR")"
 VENV_DIR="$API_DIR/.venv"
 
-apt-get install -y python3 python3-module-pip python3-module-virtualenv talosctl nmap yq
-
+apt-get install -y python3 python3-module-pip talosctl nmap yq
 
 python3 -m venv "$VENV_DIR"
-# shellcheck disable=SC1091
 . "$VENV_DIR/bin/activate"
 pip install -U pip
 pip install -e "$API_DIR"
